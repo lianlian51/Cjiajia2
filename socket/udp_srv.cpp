de8b08314f6d7d15ce1931683785b7f75a4bfba5
lianlian51 <3282125330@qq.com>
2020-10-20 15:58:30 +0800

    服务端

--- socket/udp_srv.cpp
+++ socket/udp_srv.cpp
@@ -1,38 +1,39 @@
-// 封装一个UdpSocket类，每一个实例化对象都可以通过成员接口实现一个客户端或服务端
-// 也就意味着每个实例化对象都有自己的套接字描述符
+#include "udpsocket.hpp"
+#include <iostream>
 
-#include <cstdio>
-#include <cstdlib>
-#include <unistd.h>
-#include <string>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-
-class UdpSocket{
-  private:
-    int _socket;
-  public:
-    UdpSocket();
-    bool Socket(); // 创建套接字
-    bool Bind(const std::string &ip, const uint16_t port); // 绑定地址信息
-    // 接收数据，并且根据默认参数是否为NULL，选择是否获取对端地址信息
-    bool Recv(const std::string *buf, std::string *ip = NULL, uint16_t *port = NULL);
-    bool Sent(const std::string &data, const std::string &ip, const uint16_t port);
-    bool Close(); // 关闭套接字
-};
-
-int main()
+#define CHECK_RET(q) if((q) == false){return -1;}
+int main(int argc, char* argv[])
 {
-  UdpSocket sock;
-  sock.Socket();
-  sock.Bind("192.168.122.132", 9000);
-  while(1)
+  if(argc != 3)
   {
-    std::string buf;
-    sock.Recv(&buf);
+    std::cout << "usage: ./udp_srv 192.168.122.132 9000\n";
+    return -1;
+  }
+  std::string ip = argv[1];
+  uint16_t port = std::stoi(argv[2]); // c++11的接口
 
-    sock.Sent(buf, "192.168.122.132", 8000);
+  UdpSocket sock;
+  CHECK_RET(sock.Socket());
+  CHECK_RET(sock.Bind(ip, port));
+  while(1){
+    std::string buf;
+    std::string cli_ip;
+    uint16_t cli_port;
+    if(sock.Recv(&buf, &cli_ip, &cli_port) == false)
+    {
+      sock.Close();
+      return -1;
+    }
+    std::cout<<"client say: " << buf << std::endl;
+    buf.clear();
+    std::cout << "server say: ";
+    std::cin >> buf;
+    if(sock.Sent(buf, cli_ip, cli_port) == false)
+    {
+      sock.Close();
+      return -1;
+    }
   }
   sock.Close();
+  return 0;
 }
