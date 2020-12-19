4c658f48dfd3e9b5f95763e2c4ba6415784616c4
lianlian51 <3282125330@qq.com>
2020-12-19 16:32:55 +0800

    二叉树

--- algorithm/bintree/treenode.hpp
+++ algorithm/bintree/treenode.hpp
@@ -2,24 +2,27 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <malloc.h>
 #include <assert.h>
 
 #include <iostream>
+#include <queue>
 
-typedef int DataType;
+typedef char DataType;
 
-struct BinTreeNode
+typedef struct BTNode
 {
   DataType data_;
-  struct BinTreeNode* left;
-  struct BinTreeNode* right;
-};
+  struct BTNode* left_;
+  struct BTNode* right_;
+}BTNode;
 
+typedef BTNode* pBTNode;
 
-BinTreeNode* BuyNode(DataType data)
+BTNode* BuyNode(DataType data)
 {
-  BinTreeNode* newNode = (BinTreeNode*)malloc(sizeof(BinTreeNode));
+  BTNode* newNode = (BTNode*)malloc(sizeof(BTNode));
 
   if(NULL == newNode)
   {
@@ -27,101 +30,126 @@ BinTreeNode* BuyNode(DataType data)
     return NULL;
   }
   newNode->data_= data;
-  newNode->left = NULL;
-  newNode->right = NULL;
+  newNode->left_ = NULL;
+  newNode->right_ = NULL;
 
   return newNode;
 }
 
-BinTreeNode* Create()
+void InitBTree(pBTNode* Tree)
 {
-  BinTreeNode* node1 = BuyNode(1);
-  BinTreeNode* node2 = BuyNode(2);
-  BinTreeNode* node3 = BuyNode(3);
-  BinTreeNode* node4 = BuyNode(4);
-  BinTreeNode* node5 = BuyNode(5);
-  BinTreeNode* node6 = BuyNode(6);
+  *Tree = NULL;
+}
 
-  BinTreeNode* pRoot = node1;
-  node1->left = node2;
-  node1->right = node3;
-  node2->left = node4;
-  node2->right = node5;
-  node3->left = node6;
+void CreateBTree(pBTNode *Tree)
+{
+  DataType data;
+  std::cin >> data;
+  if(data == '#')
+  {
+   Tree = NULL;
+  }
+  else 
+  {
+    *Tree = new BTNode;
+    (*Tree)->data_ = data;
+    // (*Tree)->left_ = NULL;
+    // (*Tree)->right_ = NULL;
+    CreateBTree(&(*Tree)->left_);
+    CreateBTree(&(*Tree)->right_);
+  }
+}
+
+BTNode* Create_()
+{
+  BTNode* node1 = BuyNode(1);
+  BTNode* node2 = BuyNode(2);
+  BTNode* node3 = BuyNode(3);
+  BTNode* node4 = BuyNode(4);
+  BTNode* node5 = BuyNode(5);
+  BTNode* node6 = BuyNode(6);
+
+  BTNode* pRoot = node1;
+  node1->left_ = node2;
+  node1->right_ = node3;
+  node2->left_ = node4;
+  node2->right_ = node5;
+  node3->left_ = node6;
 
   return pRoot;
+
 }
 
 // 前序遍历
-void PreOrder(BinTreeNode* root)
+void PreOrder(pBTNode Tree)
 {
-  if(root)
+  if(Tree)
   {
-    printf("%d ", root->data_);
-    PreOrder(root->left);
-    PreOrder(root->right);
+    std::cout << Tree->data_ << "->";
+    PreOrder(Tree->left_);
+    PreOrder(Tree->right_);
   }
 }
 
 // 中序遍历
-void InOrder(BinTreeNode* root)
+void InOrder(pBTNode Tree)
 {
-  if(root)
+  if(Tree)
   {
-    InOrder(root->left);
-    printf("%d ", root->data_);
-    InOrder(root->right);
+    InOrder(Tree->left_);
+    std::cout << Tree->data_ << "->";
+    InOrder(Tree->right_);
   }
 }
 
 // 后序遍历
-void PostOrder(BinTreeNode* root)
+void PostOrder(pBTNode Tree)
 {
-  if(root)
+  if(Tree)
   {
-    PostOrder(root->left);
-    PostOrder(root->right);
-    printf("%d ", root->data_);
+    PostOrder(Tree->left_);
+    PostOrder(Tree->right_);
+    std::cout << Tree->data_ << "->";
   }
 }
 
 // 获取节点的个数
-int GetCount(BinTreeNode* root)
+int GetCount(BTNode* root)
 {
-  assert(root);
-  if(root->left == NULL && root->right == NULL)
+  // assert(root);
+  if(root == NULL)
     return 0;
 
-  return 1 + GetCount(root->left) + GetCount(root->right);
+  return 1 + GetCount(root->left_) + GetCount(root->right_);
 }
 
 // 获得叶子节点的个数
-int GetleafCount(BinTreeNode* root)
+int GetleafCount(BTNode* root)
 {
   if(root == NULL)
     return 0;
 
-  if(root->left == NULL && root->right == NULL)
+  if(root->left_ == NULL && root->right_ == NULL)
     return 1;
 
-  return GetleafCount(root->left) + GetleafCount(root->right);
+  return GetleafCount(root->left_) + GetleafCount(root->right_);
 }
 
 // 返回二叉树的高度
-int GetHeight(BinTreeNode* root)
+int GetHeight(BTNode* root)
 {
   assert(root);
   if(NULL == root)
     return 0;
 
-  int leftHeight = GetHeight(root->left);
-  int rightHeight = GetHeight(root->right);
+  int leftHeight = GetHeight(root->left_);
+  int rightHeight = GetHeight(root->right_);
 
   return leftHeight > rightHeight ? leftHeight : rightHeight;
 }
 
 // 获取k层节点的个数
-int GetKCount(BinTreeNode* root, int k)
+int GetKCount(BTNode* root, int k)
 {
   if(root == NULL)
     return 0;
@@ -129,42 +157,65 @@ int GetKCount(BinTreeNode* root, int k)
   if(k == 1)
     return 1;
 
-  return GetKCount(root->left, k - 1) + GetKCount(root->right, k - 1); 
+  return GetKCount(root->left_, k - 1) + GetKCount(root->right_, k - 1); 
 }
 
 // 返回x在二叉树中的节点
-BinTreeNode* Find(BinTreeNode* root, DataType data)
+BTNode* Find(BTNode* root, DataType data)
 {
   if(root == NULL)
     return NULL;
 
-  BinTreeNode* pNode = NULL;
+  BTNode* pNode = NULL;
 
   if(root->data_ == data)
     return root;
 
-  if((pNode = Find(root->left, data)) || (pNode = Find(root->right, data)))
+  if((pNode = Find(root->left_, data)) || (pNode = Find(root->right_, data)))
     return pNode;
 
   return NULL;
 }
 
 // 获取节点的双亲
-BinTreeNode* GetParent(BinTreeNode* root, BinTreeNode* node)
+BTNode* GetParent(BTNode* root, BTNode* node)
 {
   if(root == NULL || node == NULL || root == node)
     return NULL;
 
-  BinTreeNode* pParent = NULL;
+  BTNode* pParent = NULL;
 
-  if(root->left == node || root->right == node)
+  if(root->left_ == node || root->right_ == node)
     return root;
 
-  if((pParent = GetParent(root->left, node)) || (pParent = GetParent(root->right, node)))
+  if((pParent = GetParent(root->left_, node)) || (pParent = GetParent(root->right_, node)))
     return pParent;
 
   return NULL;
 }
 
 // 二叉树层序遍历
+void levelOrder_queue(pBTNode& Tree)
+{
+  std::queue<pBTNode> q;
+  if(Tree != NULL)
+  {
+    q.push(Tree);
+  }
 
+  while(!q.empty())
+  {
+    std::cout << q.front()->data_ << "->";
+
+    if(q.front()->left_)
+    {
+      q.push(q.front()->left_);
+    }
+
+    if(q.front()->right_)
+    {
+      q.push(q.front()->right_);
+    }
+    q.pop();
+  }
+}
